c924f3451a62bfd3ec8f818ebeaeba5b0217b9f2
J_BO <joobo95@gmail.com>
2020-10-17 23:47:50 +0900

    [인프런 - 스프링 입문] 스프링 DB 접근 기술

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,27 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+    private EntityManager em;
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource, EntityManager em, MemberRepository memberRepository) {
+        this.dataSource = dataSource;
+        this.em = em;
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +30,10 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+        return memberRepository;
     }
 }
