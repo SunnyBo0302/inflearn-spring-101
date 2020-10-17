d2fa75c3cb8e9e1de8052dcc8c2339fc33bd45c6
J_BO <joobo95@gmail.com>
2020-10-18 00:37:21 +0900

    [인프런 - 스프링 입문] AOP

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,25 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component //config에서 @Bean으로 등록하는 것이 눈에 띄어서 추천한다고 함.
+public class TimeTraceAop {
+
+    @Around("execution(* hello.hellospring..*(..))") // hello.hellospring 패키지 하위 클래스 모두
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+
+}
